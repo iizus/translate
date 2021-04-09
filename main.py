7cb53925270175ad230747645f068ca81f8d3447
Shota Iizuka <shota.iizuka@icloud.com>
2021-04-09 13:07:23 +0000

    add box

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import PySimpleGUI as gui
 
+title = 'Translate'
+width = 80
+height = 30
+
+box_size = (width, height)
+left_box = gui.Multiline(size=box_size)
+right_box = gui.Multiline(size=box_size)
+
 layout = [
-    [gui.InputText(), gui.InputText()]
+    [left_box, right_box]
 ]
 
-win = gui.Window('', layout)
+window = gui.Window(title, layout)
 
 while True:
-  event, values = win.read()
+	event, values = window.read()
+	if event is None: break
+else:
+	window.close()
