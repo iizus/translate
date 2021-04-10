d7a0703e8bb92fb69b2325e20f88b12e8a83a3a8
Shota Iizuka <shota.iizuka@icloud.com>
2021-04-10 00:37:45 +0000

    add timeout

--- main.py
+++ main.py
@@ -2,23 +2,33 @@ import PySimpleGUI as gui
 
 title = 'Translate'
 width = 80
-height = 30
+height = 10
+timeout = 100
 
 box_size = (width, height)
 
-def make_box():
-	box = gui.Multiline(size=box_size)
+def make_box(text, key):
+	box = gui.Multiline(text, key=key, size=box_size)
 	return box
 
-languages = ['choice 1', 'choice 2']
+# languages = ['choice 1', 'choice 2']
 
-layout = [
-	[],
-    [make_box(), make_box()]
-]
+left_arrow = gui.Text('<-')
+right_arrow = gui.Text('->')
+arrow = right_arrow
 
+def make_layout(text):
+	layout = [
+		[make_box(text, key='left_box'), arrow, make_box(text, key='right_box')]
+	]
+	return layout
+
+text = ''
+layout = make_layout(text)
 window = gui.Window(title, layout)
 
 while True:
-	event, values = window.read()
+	event, values = window.read(timeout=timeout)
+	print(event, values)
+
 	if event is None: break
