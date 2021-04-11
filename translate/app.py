2a80ae694e1781d1ff4596bed28081d3d6d7b8f6
Shota Iizuka <shota.iizuka@icloud.com>
2021-04-11 12:40:40 +0000

    add func

--- translate/app.py
+++ translate/app.py
@@ -1,8 +1,25 @@
 from chalice import Chalice
-from boto3 import client
-
 app = Chalice(app_name='translate')
+
+region = 'ap-northeast-1'
+
+from boto3 import client
+translate = client(
+    service_name = 'translate',
+    region_name = region,
+    use_ssl = True,
+)
+
+def translate_to_japanese(text):
+    response = translate.translate_text(
+        Text = text,
+        SourceLanguageCode = 'auto',
+        TargetLanguageCode = 'ja',
+    )
+    return response
+
 
 @app.route('/')
 def index():
-    return {'hello': 'world1'}
+    response = translate_to_japanese('hello')
+    return response
