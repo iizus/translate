6a44d64bf94f3efca9c62e2ff6f44b4df0739abe
Shota Iizuka <shota.iizuka@icloud.com>
2021-04-11 13:11:21 +0000

    success

--- translate/app.py
+++ translate/app.py
@@ -1,25 +1,50 @@
 from chalice import Chalice
 app = Chalice(app_name='translate')
 
-region = 'ap-northeast-1'
+REGION = 'ap-northeast-1'
+
+ENGLISH = 'en'
+JAPANESE = 'ja'
 
 from boto3 import client
-translate = client(
+translate_client = client(
     service_name = 'translate',
-    region_name = region,
+    region_name = REGION,
     use_ssl = True,
 )
 
-def translate_to_japanese(text):
-    response = translate.translate_text(
+def translate_to(language, text):
+    response = translate_client.translate_text(
         Text = text,
         SourceLanguageCode = 'auto',
-        TargetLanguageCode = 'ja',
+        TargetLanguageCode = language,
     )
-    return response
+    translated_text = response.get('TranslatedText')
+    source_language = response.get('SourceLanguageCode')
+    return translated_text, source_language
 
 
-@app.route('/')
-def index():
-    response = translate_to_japanese('hello')
-    return response
+def translate_to_japanese(text):
+    translated_text, source_language = translate_to(JAPANESE, text)
+    return translated_text, source_language
+
+
+def translate_to_english(text):
+    translated_text, source_language = translate_to(ENGLISH, text)
+    return translated_text, source_language
+
+
+def translate(text):
+    translated_text, source_language = translate_to_japanese(text)
+    if source_language == JAPANESE:
+        translated_text, _ = translate_to_english(text)
+    return translated_text
+
+
+from urllib import parse
+
+@app.route('/{text}', methods=['GET'])
+def index(text):
+    decoded_text = parse.unquote(text)
+    translated_text = translate(decoded_text)
+    return translated_text
