204e1e5ca55de8121ba1d2e92638b9080640db46
Shota Iizuka <shota.iizuka@icloud.com>
2021-04-17 00:56:29 +0000

    fix japanese

--- translate/app.py
+++ translate/app.py
@@ -5,11 +5,28 @@ from chalicelib import aws
 from urllib import parse
 
 @app.route(
-    '/{text}',
-    methods = ['GET'],
+    '/',
+    methods = ['PUT'],
     api_key_required = True,
 )
-def translate(text):
-    decoded_text = parse.unquote(text)
-    translated_text = aws.translate(decoded_text)
-    return translated_text
+def translate():
+    request = app.current_request
+    # text = request.json_body
+    text = request.raw_body
+    # dic = request.to_dict()
+    # text = str(text)
+    # text = parse.unquote(text)
+    # print(dic)
+    print(text.decode())
+    return text
+    # return text
+    # text = str(text)
+    # text = parse.quote(text)
+    # decoded_text = parse.unquote(text)
+    # translated_text = aws.translate(text)
+    # translated_text = str(translated_text)
+    # translated_text = parse.unquote(translated_text)
+    # translated_text = str(translated_text)
+    # return translated_text
+    # translated_text = str(translated_text)
+    # return str(type(translated_text))
